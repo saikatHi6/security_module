e240778bac3908c9ee3c2268af3a46fdecfcdc6b
Saikat Sadhukhan (c) <ssadhukhan@vmware.com>
2020-05-03 02:28:04 +0530

    [00]- custom security configaration

---
+++ src/test/java/org/atom/login/SecurityModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.atom.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
