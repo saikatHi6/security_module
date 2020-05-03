06dbfedb5b03cc40b92ac2d13c58e7fdccb58e66
Saikat Sadhukhan (c) <ssadhukhan@vmware.com>
2020-05-03 15:44:11 +0530

    [03] - Exception handaling

--- src/main/java/org/atom/login/config/WebSecurityConfig.java
+++ src/main/java/org/atom/login/config/WebSecurityConfig.java
@@ -1,10 +1,12 @@
 package org.atom.login.config;
 
+import org.atom.login.exception.JwtAuthenticationEntryPoint;
 import org.atom.login.filters.JwtRequestFilter;
 import org.h2.server.web.WebServlet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -24,36 +27,47 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	private JwtRequestFilter jwtRequestFilter;
 
 	@Autowired
+	private JwtAuthenticationEntryPoint unAuthorized;
+	
+	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(myUserDetailsService);
+		auth.userDetailsService(myUserDetailsService).passwordEncoder(passwordEncoder());
 	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
+		return new BCryptPasswordEncoder();
 	}
 
 	@Override
-	@Bean
+	@Bean(BeanIds.AUTHENTICATION_MANAGER)
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		return super.authenticationManagerBean();
 	}
 
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.csrf().disable()
-		.authorizeRequests().antMatchers("/","/h2/**","/authenticate").permitAll().
+		httpSecurity.
+		cors().disable().
+		csrf().disable().
+		authorizeRequests().antMatchers("/","/h2/**","/authenticate").permitAll().
 		anyRequest().authenticated().and().
-		exceptionHandling().and().sessionManagement()
+		exceptionHandling().
+		authenticationEntryPoint(unAuthorized).
+		and().sessionManagement()
 		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
+		
+		//Config for H2 DB only 
 		httpSecurity.headers().frameOptions().disable();
 		
 	}
 	
 	
-	
+	/*
+	 * Config for H2 DB only
+	 * */
 	@Bean
     ServletRegistrationBean h2servletRegistration(){
         ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
